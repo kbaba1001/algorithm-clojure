5153e5f4c741aeb4e572cf06952970e3719b1968
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-10-14 15:34:25 +0900

    selection sort

---
+++ sorts/selection_sort.clj
@@ -0,0 +1,12 @@
+;; https://en.wikipedia.org/wiki/Selection_sort
+
+(defn selection-sort [coll]
+  (loop [input coll
+         result []]
+    (if (= (count coll) (count result))
+      result
+      (let [min (apply min input)
+            rest (filter #(not= min %) input)]
+        (recur rest (conj result min))))))
+
+(println (selection-sort [5 4 8 7 9 3 1]))
