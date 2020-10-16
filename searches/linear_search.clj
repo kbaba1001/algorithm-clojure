135de111c48b375107b03b43bc65c8263dd7db91
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-10-16 13:48:17 +0900

    use reduced

--- searches/linear_search.clj
+++ searches/linear_search.clj
@@ -1,12 +1,21 @@
 ;; https://en.wikipedia.org/wiki/Linear_search
 
+;; (defn linear-search [coll target]
+;;   (let [i (atom 0)]
+;;     (while (and (< @i (count coll)) (not= (coll @i) target))
+;;       (swap! i inc))
+;;     (if (= @i (count coll))
+;;       -1
+;;       @i)))
+
 (defn linear-search [coll target]
-  (let [i (atom 0)]
-    (while (and (< @i (count coll)) (not= (coll @i) target))
-      (swap! i inc))
-    (if (= @i (count coll))
-      -1
-      @i)))
+  (reduce
+   (fn [_ [i x]]
+     (if (= x target)
+       (reduced i)
+       -1))
+   0
+   (map-indexed vector coll)))
 
 (println (= (linear-search [0 5 7 10 15] 0) 0))
 (println (= (linear-search [0 5 7 10 15] 15) 4))
