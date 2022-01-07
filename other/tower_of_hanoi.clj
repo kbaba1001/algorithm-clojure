252f45c000e870f186cb8e31654750d0ad598624
kbaba1001 <kbaba1001@users.noreply.github.com>
2022-01-08 06:53:03 +0900

    feat: use stack in babashka

--- other/tower_of_hanoi.clj
+++ other/tower_of_hanoi.clj
@@ -1,19 +1,27 @@
-;; TODO I want to run this in Babashka, but I can't use java.util.Stack in the environment.
+#!/usr/bin/env bb
 
 ;; https://en.wikipedia.org/wiki/Tower_of_Hanoi
 
+;; I was not able to use pop! or java.util.Stack in Babashka :/
+(defn pop!! [stack-atom]
+  (let [x (first @stack-atom)]
+    (swap! stack-atom next)
+    x))
+(defn push!! [stack-atom value]
+  (reset! stack-atom (cons value @stack-atom)))
+
 (defn hanoi
   ([n]
-   (let [a (java.util.Stack.)]
+   (let [a (atom [])]
      (doseq [i (reverse (range 1 (inc n)))]
-       (.push a i))
-     (hanoi n 1 [a (java.util.Stack.) (java.util.Stack.)])))
+       (push!! a i))
+     (hanoi n 1 [a (atom []) (atom [])])))
   ([n m [a b c :as arr]]
-   (printf "A: %s\nB: %s\nC: %s\n--------\n" a b c)
-   (when (< (.size c) n)
+   (printf "A: %s\nB: %s\nC: %s\n--------\n" @a @b @c)
+   (when (< (count @c) n)
      (let [from-stack (get arr (mod (bit-and m (dec m)) 3))]
-       (when-not (.empty from-stack)
-         (.push (get arr (mod (inc (bit-or m (dec m))) 3)) (.pop from-stack)))
+       (when (seq @from-stack)
+         (push!! (get arr (mod (inc (bit-or m (dec m))) 3)) (pop!! from-stack)))
        (recur n (inc m) arr)))))
 
-(hanoi 2)
+(hanoi 3)
--- sorts/quick_sort3.clj
+++ sorts/quick_sort3.clj
@@ -1,23 +1,31 @@
-;; TODO I want to run this in Babashka, but I can't use java.util.Stack in the environment.
+#!/usr/bin/env bb
+
+;; I was not able to use pop! or java.util.Stack in Babashka :/
+(defn pop!! [stack-atom]
+  (let [x (first @stack-atom)]
+    (swap! stack-atom next)
+    x))
+(defn push!! [stack-atom value]
+  (reset! stack-atom (cons value @stack-atom)))
 
 ;; https://en.wikipedia.org/wiki/Quicksort
 (defn qsort [arr]
-  (let [stack (new java.util.Stack)]
-    (with-local-vars [result []]
-      (.push stack arr)
-      (while (not (.empty stack))
-        (let [target (.pop stack)
-              pivot (first target)
-              res (next target)
-              left (filter #(< % pivot) res)
-              right (filter #(>= % pivot) res)]
-          (if (and (empty? left) (empty? right))
-            (var-set result (cons pivot @result))
-            (do
-              (.push stack left)
-              (.push stack [pivot])
-              (.push stack right)))))
-      (remove nil? @result))))
+  (let [stack (atom [])
+        result (atom [])]
+    (push!! stack arr)
+    (while (seq @stack)
+      (let [target (pop!! stack)
+            pivot (first target)
+            res (next target)
+            left (filter #(< % pivot) res)
+            right (filter #(>= % pivot) res)]
+        (if (and (empty? left) (empty? right))
+          (push!! result pivot)
+          (do
+            (push!! stack left)
+            (push!! stack [pivot])
+            (push!! stack right)))))
+    (remove nil? @result)))
 
 (qsort [3,2,5,4,1,2])
 ;; => (1 2 2 3 4 5)
