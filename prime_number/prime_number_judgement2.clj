6aaa3a66b522a4ec5ef85ab4989d7219f216ae59
kbaba1001 <kbaba1001@users.noreply.github.com>
2022-01-08 14:58:18 +0900

    feat: add prime number judgement2

---
+++ prime_number/prime_number_judgement2.clj
@@ -0,0 +1,25 @@
+(ns prime-number.prime-number-judgement2
+  (:require [clojure.test :as t]))
+
+;; If there is a number that can devide `n` between 2 to `(Math/floor (Math/sqrt n))`, then `n` is not a prime number. 
+(defn is-prime [n]
+  (loop [i 2]
+    (cond
+      (< n (* i i))
+      true
+
+      (zero? (mod n i))
+      false
+
+      :else
+      (recur (inc i)))))
+
+(t/deftest is-prime-test
+  (t/are [number result] (t/is (= (is-prime number) result))
+    2 true
+    4 false
+    5 true
+    52 false
+    53 true))
+
+(t/run-tests)
